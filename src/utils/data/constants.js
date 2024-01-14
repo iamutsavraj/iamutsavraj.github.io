7653286f5f944e0229bc9db8e9de833f5c89e62c
Johncodingsnow <iamutsavraj@gmail.com>
2024-01-15 00:02:17 +0530

    first commit

---
+++ src/utils/data/constants.js
@@ -0,0 +1,411 @@
+
+import CognoRise from "../../images/CognoRise.jpg"
+import psyliqlog from "../../images/pysliqlogo.jpg"
+import CognoRiselog from "../../images/CognoRiselogo.jpg"
+import Codingsamurailogo from "../../images/Codingsamurailogo.jpg"
+import Codingsamurai from "../../images/Codingsamurai.jpg"
+import MeriSkilllogo from "../../images/MeriSkilllogo.jpg"
+import Meriskill1 from "../../images/Meriskill1.jpg"
+import Hnb from "../../images/Hnb.jpg"
+import school from "../../images/school.jpg"
+import callcentrep from "../../images/callcentrep.pdf"
+import callcentrej from "../../images/callcentrej.jpg"
+import unempp from "../../images/unempp.pdf"
+import umempj from "../../images/unempj.jpg"
+import crj from "../../images/crj.jpg"
+import crp from "../../images/crp.pdf"
+import eaj from "../../images/eaj.jpg"
+import eap from "../../images/eap.pdf"
+import dij from "../../images/dij.jpg"
+import dip from "../../images/dip.pdf"
+import sdj from "../../images/sdj.jpg"
+import sdp from "../../images/sdp.pdf"
+import hrj from "../../images/hrj.jpg"
+import hrp from "../../images/hrp.pdf"
+import haj from "../../images/haj.jpg"
+import hap from "../../images/hap.pdf"
+import pmj from "../../images/pmj.jpg"
+import pmp from "../../images/pmp.pdf"
+import pyn from "../../images/pyn.png"
+import psql from "../../images/psql.png"
+import msex from "../../images/msex.png"
+import msw from "../../images/msw.png"
+import gs from "../../images/gs.png"
+import bi from "../../images/bi.png"
+import sqlnn from "../../images/sqlnn.svg"
+import oracle from "../../images/oracle.png"
+import mspp from "../../images/mspp.png"
+import pysliqlor from "../../images/pysliqlor.jpg"
+
+export const Bio = {
+    name: "Utsav Raj",
+    roles: [
+      "Data Analyst",
+      "Business Analyst",
+      "Power BI Developer",
+      "Data Entry Operator",
+    ],
+    description:
+      "I am an Enthusiastic learner with a keen interest in Analysis. As a dedicated and motivated individual, I am eager to start my career and take it to new heights with my continuous learning behaviour and positive attitude. I thrive in challenging and ever changing business environments and have the ability to adapt and evolve as necessary.",
+    github: "https://github.com/iamutsavraj",
+    resume:
+      "https://drive.google.com/file/d/1XSn5vRGKRR3T_9fUX3Iy9zeQ5fRKL33I/view?usp=drive_link",
+    linkedin: "https://www.linkedin.com/in/iamutsavraj/",
+    twitter: "https://twitter.com/iamutsavraj",
+    insta: "https://www.instagram.com/iamutsavraj/",
+  };
+  
+  export const skills = [
+    
+    {
+      title: "Analytical Tools",
+      skills: [
+        {
+          name: "Power BI",
+          image:bi,
+        },
+        {
+          name: "Advanced Excel",
+          image: msex,
+        },
+        {
+          name: "Google Sheet",
+          image: gs,
+        },
+        {
+          name: "Microsoft Word",
+          image: msw,
+        },
+        {
+          name: "MS Power Point",
+          image: mspp,
+        },
+      ],
+    },
+    
+    {
+      title: "Programming Language & Databases",
+      skills: [
+        {
+          name: "Python",
+          image: pyn,
+        },
+        {
+          name: "MySQL",
+          image: sqlnn,
+        },
+        {
+          name: "Postgresql",
+          image: psql,
+        },
+        {
+          name: "Oracle",
+          image: oracle,
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: psyliqlog,
+      role: "Python R Intern",
+      company: "Pysliq",
+      date: "Jan 2024 - Present",
+      desc: " Currently working at psyliq as Python intern, doing intership tasks.",
+      skills: [
+        "Python",
+        "Programming Language",
+        "Dynamic Language",
+      ],
+    },
+    {
+      id: 0,
+      img: psyliqlog,
+      role: "SQL Intern",
+      company: "Pysliq",
+      date: "Dec 2023 - Jan 2024",
+      desc: "During my internship at pysliq as SQL developer, I have worked on 3 projects: HR Data Analysis, Paytm E-Purchase Data Analysis and Pharma Data Analysis which gives me a good experience working with MySQL.",
+      skills: [
+        "MySQL",
+        "Databases",
+        "SQL",
+      ],
+      doc:pysliqlor
+    },
+    {
+      id: 1,
+      img: CognoRiselog,
+      role: "Data Analyst Intern",
+      company: "CognoRise",
+      date: "Nov 2023 - Dec 2023",
+      desc: " Conducted comprehensive data analytics internship analyzing India’s COVID- 19 unemployment rate. Examined State- wise,  monthly, rural/urban, identifying trends. Extracted insights on employed populations across regions for decision- making.",
+      skills: [
+        "Power BI",
+        "Data Cleaning",
+        "Statistics",
+        "Data Visualization",
+      ],
+      doc:CognoRise
+    },
+    {
+      id: 2,
+      img: Codingsamurailogo,
+      role: "Business Analyst",
+      company: "Coding Samurai",
+      date: "Nov 2023 - Dec 2023",
+      desc: "Conducted impactful Business Analytics internship focusing on Employee Attrition Analysis. Analyzed employee data to understand  the factors that contribute to employee attrition and identifying the root cause that can help in retention effort.",
+      skills: [
+        "Power BI",
+        "Data Visualization",
+        "Business Analysis",
+        "Data Cleaning",
+      ],
+      doc:Codingsamurai
+    },
+    
+    {
+      id: 3,
+      img: MeriSkilllogo,
+      role: "Data Analyst Intern",
+      company: "MeriSkill",
+      date: "Oct 2023 - Nov 2023",
+      desc: "Sales Data Analysis: Analyze sales data to identify trends, top-selling products, and revenue metrics such as total sales  and profit margins, and created visualization for business decision- making.\n\n  HR Analytics: Analyzed employes data to visualize data such as marital status, job role, gender, education field,  department, working year etc. to optimize talent management and organizational performance.",
+      skills: [
+        "Data Visualization",
+        "Power BI Dashboards",
+        "Statistics",
+        "Data Cleaning",
+        
+      ],
+      doc: Meriskill1,
+      
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: Hnb,
+      school: "Hemvati Nandan Bahuguna Garhwal University (A Central University), Dehradun",
+      date: "Jun 2020 - Oct 2023",
+      grade: "7.24 CGPA",
+      desc: "I recently graduated from HNB Garhwal University with a passion for exploring the realms of Data Analytics and Business Analytics. Leveraging my strong foundation in mathematics & statistics, I am actively engaged in deep analysis to extract meaningful insights. My academic journey has equipped me with the skills and knowledge needed to navigate the dynamic landscape of analytics, and I am committed to continuous learning and growth in these fields.",
+      degree: "Bachelor's degree - Mathematics",
+    },
+    {
+      id: 1,
+      img: school,
+      school: "Constancia School, Dehradun",
+      date: "Mar 2019 - Mar 2020",
+      grade: "81.20%",
+      desc: "I have completed intermediate from Constancia School, Dehradun where I studied PCM with Computer Science.",
+      degree: "ISC(XII), PCM with Computer Science",
+    },
+    {
+      id: 2,
+      img: school,
+      school: "Constancia School, Dehradun",
+      date: "Mar 2017 - Mar 2018",
+      grade: "83.33%",
+      desc: "I have completed High school from Constancia School, Dehradun where I studied Science with Computer Application.",
+      degree: "ICSC(X), Science with Computer Application",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 3,
+      title: "Diversion & Inculsion Dashboard ",
+      date: "Jan 2024",
+      description:
+        "Analyzed employee data to understand the factors that contribute to employee attrition and identifying the root cause that can help in retention effort.",
+      image:dij,
+      tags: [
+        "Power BI Desktop",
+        "KPIs",
+        "Data Cleaning",
+        "Attrition",
+        "DAX",
+        "Dashboard",
+       
+      ],
+      category: "Power BI",
+      github: "https://github.com/iamutsavraj/Coding-Samurai-Internship-Task-Employee-Attrition",
+      pdf: dip,
+      pre: "https://www.linkedin.com/posts/iamutsavraj_powerbi-dataanalysis-pwcindia-activity-7152184596204261376-MKcZ?utm_source=share&utm_medium=member_desktop",
+    },
+    {
+      id: 1,
+      title: "Unemployment Rate Analysis in India During COVID- 19",
+      date: "Dec 2023",
+      description:
+        "Analyzed the unemployment rate in India during COVID- 19 and to identify unemployment rate by states, year & month, area (rural or urban); and average employed people by states and areas. Analyzed the unemployment rate as well as average employed people in each state to identified which state has suffered the most during COVID – 19.",
+      image:umempj,
+      tags: [
+        "Power BI Desktop",
+        "Statistics",
+        "Data Cleaning",
+        "Data Visualization",
+        "DAX",
+        "Dashboard",
+       
+      ],
+      category: "Power BI",
+      github: "https://github.com/iamutsavraj/Unemployement-In-India-during-COVID19-PowerBI-Project",
+      pdf: unempp,
+      pre: "https://www.linkedin.com/posts/iamutsavraj_dataanalysis-internshipexperience-unemploymenttrends-activity-7136757348487901184-TOWr?utm_source=share&utm_medium=member_desktop",
+    },
+    {
+      id: 0,
+      title: "Call Centre Analysis",
+      date: "Dec 2023",
+      description:
+        "Completed this project during the PwC Power BI Virtual Case Experience on Forage. Created a dashboard in Power BI for visualizing relevant KPIs and metrics in the dataset provided. Respond the client’s request by providing a well- design Power BI dashboard reflecting the requested KPIs.",
+      image:callcentrej,
+      tags: [
+        "Power BI Desktop",
+        "Data Cleaning",
+        "Data Visualization",
+        "DAX",
+        "Analysis",
+       
+      ],
+      category: "Power BI",
+      github: "https://github.com/iamutsavraj/Call-Centre-Analysis-PwC-PowerBI-Project",
+      pdf: callcentrep,
+      pre: "https://www.linkedin.com/posts/iamutsavraj_pwc-forage-powerbi-activity-7139473102270373888-QqqI?utm_source=share&utm_medium=member_desktop",
+    },
+    {
+      id: 2,
+      title: "Customer Retention Analysis",
+      date: "Dec 2023",
+      description:
+        "In this project I have defined proper KPIs, created a churn dashboard using the defined KPIs to reflect customer demographics, Customer account information, and other insights. Also created a Customer Risk Analysis Dashboard to reflect risk rate, yearly charges, churn by payment method etc.",
+      image:crj,
+      tags: [
+        "Power BI Desktop",
+        "KPIs",
+        "Data Cleaning",
+        "Customer Retention",
+        "DAX",
+        "Dashboard",
+       
+      ],
+      category: "Power BI",
+      github: "https://github.com/iamutsavraj/Customer-Retention-PwC-PowerBI-Project",
+      pdf: crp,
+      pre: "https://www.linkedin.com/posts/iamutsavraj_forage-datavisualization-pwc-activity-7141753510643052544-XDol?utm_source=share&utm_medium=member_desktop",
+    },
+    {
+      id: 3,
+      title: "Employee Attrition Analysis",
+      date: "Nov 2023",
+      description:
+        "Analyzed employee data to understand the factors that contribute to employee attrition and identifying the root cause that can help in retention effort.",
+      image:eaj,
+      tags: [
+        "Power BI Desktop",
+        "KPIs",
+        "Data Cleaning",
+        "Attrition",
+        "DAX",
+        "Dashboard",
+       
+      ],
+      category: "Power BI",
+      github: "https://github.com/iamutsavraj/Coding-Samurai-Internship-Task-Employee-Attrition",
+      pdf: eap,
+      pre: "https://www.linkedin.com/posts/iamutsavraj_codingsamurai-businessanalytics-dataanalytics-activity-7134883590185623552-0Joq?utm_source=share&utm_medium=member_desktop",
+    },
+    {
+      id: 7,
+      title: "Paytm Mall Epurchase Data Analysis",
+      date: "Jan 2023",
+      description:
+        "I have completed this task during my SQL internship at Psyliq.",
+      image: pmj,
+      tags: [
+        "MySQL",
+        "SQL",
+        "Databases",
+        "Presentation",
+       
+      ],
+      category: "SQL",
+      github: "https://github.com/iamutsavraj/Paytm_Mall_Epurchase_Analysis_SQL_Project",
+      pdf: pmp,
+      pre: "https://www.linkedin.com/posts/iamutsavraj_task-2-paytm-mall-e-purchase-data-analysis-activity-7147449264112087040-IXrx?utm_source=share&utm_medium=member_desktop",
+    },
+    {
+      id: 6,
+      title: "HR Analysis",
+      date: "Dec 2023",
+      description:
+        "I have completed this task during my SQL internship at Psyliq.",
+      image: haj,
+      tags: [
+        "MySQL",
+        "SQL",
+        "Databases",
+        "Presentation",
+       
+      ],
+      category: "SQL",
+      github: "https://github.com/iamutsavraj/HR_data_analysis_SQL_Project",
+      pdf: hap,
+      pre: "https://www.linkedin.com/posts/iamutsavraj_task-1-hr-analysis-activity-7144892011362332672-irSr?utm_source=share&utm_medium=member_desktop",
+    },
+    {
+      id: 4,
+      title: "Sales Data Analysis",
+      date: "Oct 2023",
+      description:
+        "I have completed this task during my Data Analytic internship at MeriSKILL. In this project I haveanalyzed sales data to identify trends, top-selling products, and revenue metrics such as total sales and profit margins, and created visualization for business decision- making.",
+      image:sdj,
+      tags: [
+        "Power BI Desktop",
+        "KPIs",
+        "Data Cleaning",
+        "sales analysis",
+        "DAX",
+        "Dashboard",
+       
+      ],
+      category: "Power BI",
+      github: "https://github.com/iamutsavraj/MeriSkill_internship_task/tree/main",
+      pdf: sdp,
+      pre: "https://www.linkedin.com/posts/iamutsavraj_dataanalysis-powerbi-internshipexperience-activity-7127136037155319808-JtEx?utm_source=share&utm_medium=member_desktop",
+    },
+    {
+      id: 5,
+      title: "HR Analytics",
+      date: "Oct 2023",
+      description:
+        "I have completed this task during my Data Analytic internship at MeriSKILL. In this project I have analyzed employes data to visualize data such as marital status, job role, gender, education field, department, working year etc. to optimize talent management and organizational performance.",
+      image: hrj,
+      tags: [
+        "Power BI Desktop",
+        "KPIs",
+        "Data Cleaning",
+        "HR analysis",
+        "DAX",
+        "Dashboard",
+       
+      ],
+      category: "Power BI",
+      github: "https://github.com/iamutsavraj/MeriSkill_internship_task/tree/main",
+      pdf: hrp,
+      pre: "https://www.linkedin.com/posts/iamutsavraj_dataanalytics-hranalytics-powerbi-activity-7127603430805676032-iPGM?utm_source=share&utm_medium=member_desktop",
+    },
+    
+    
+    
+  ];
+  
+  export const TimeLineData = [
+    { year: 2022, text: "Started my journey" },
+    { year: 2023, text: "Started lerning data analysis and completed several internships" },
+    { year: 2024, text: "Currently learning Python" },
+  ];
